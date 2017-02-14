35040203cc0430589d688681b2b9f572a833f42a
mcattx <mcattx@gmail.com>
2017-02-14 22:19:12 +0800

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	cleanCSS = require('gulp-clean-css'),
+	plumber = require('gulp-plumber'),
+	devServer = require('gulp-devserver'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename');
+
+gulp.task('dev', []);
+
+gulp.task('build', []);
